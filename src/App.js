6d8a40100c5be9b75a13ee24fa8fe5749ab42421
azul-de-prusia <tegthyn_600@hotmail.com>
2019-09-05 16:51:31 -0500

    react-router installation, first steps to firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>Notepad</p>
+        <button type="button" class="newUser">Sign in</button>
+        <button type="button" class="bttnlogProvider">Login with facebook<img src="https://www.1-stopmedia.com/wp-content/uploads/2016/08/097668-black-ink-grunge-stamp-textures-icon-social-media-logos-facebook-logo-square.png" width="2rem" height="2rem"/></button>
+        <img src="http://animaticons.co/wp-content/uploads/2015/10/animat-pencil-512x512-color.gif" className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My note here
         </p>
         <a
           className="App-link"
